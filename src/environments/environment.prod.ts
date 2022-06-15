49fa741cb7f423213d7b66c5c06de4525ca623bc
Halaj93 <107439988+Halaj93@users.noreply.github.com>
2022-06-15 20:04:58 +0200

    skusam desktop

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'd1fea398dfmsh147f29e679e603ap193d95jsn4793e4138150'  
+};
